71fc4ec4483711e8dccabb07a58a455a054b1507
Sunline22 <craftchris640@gmail.com>
2016-10-04 22:15:08 -0500

    Added the autonomous classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAutoOne.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Autonomous variation one",group="Auto")
+public class AAutoOne extends LinearOpMode {
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAutoThree.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Autonomous variation three",group="Auto")
+public class AAutoThree extends LinearOpMode {
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
