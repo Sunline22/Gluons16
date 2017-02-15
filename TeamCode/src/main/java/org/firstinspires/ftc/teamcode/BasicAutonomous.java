9fd02ffb6cfc929fd1efa4c7a95e25cf99523d94
Sunline22 <craftchris640@gmail.com>
2017-02-14 18:41:13 -0600

    Changed widiow maker in basic autonomus to encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -43,12 +44,25 @@ public class BasicAutonomous extends LinearOpMode
         robot.backRightMotor.setPower(0);
 
         runTime.reset();
+
+        robot.cannonMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.cannonMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        final double countsPerMotorRev = 1440 ;
+        final double driveGearReduction = 2.0 ;
+        final double wheelDiameterInches = 1.75 ;
+        final double countsPerInch = (countsPerMotorRev * driveGearReduction) / (wheelDiameterInches * 3.1415);
+        int motorTarget = robot.cannonMotor.getCurrentPosition() + (int)(12 * countsPerInch);
+
+        robot.cannonMotor.setTargetPosition(motorTarget);
+        robot.cannonMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.cannonMotor.setPower(.575);
+
         while (opModeIsActive() && (runTime.seconds() < 6.0)) {
             telemetry.addData("RunTime: ", runTime);
             telemetry.addData("RunGoal: ", 4.0);
             telemetry.update();
-
-            robot.cannonMotor.setPower(.575);
 
             if(runTime.seconds()>1.9 && runTime.seconds()<2.0)
                 robot.lift.setPower(.5);
@@ -59,6 +73,8 @@ public class BasicAutonomous extends LinearOpMode
 
             idle();
         }
+
+        robot.cannonMotor.setPower(0);
 
         robot.frontLeftMotor.setPower(-.9);
         robot.backLeftMotor.setPower(-.9);
